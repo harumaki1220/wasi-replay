407db56de01ef8f103f0fd6d74f4841ea25e5b04
harumaki1220 <haruma181220@gmail.com>
2026-08-19 02:18:50 +0900

    feat: run guest component from host

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use wasmtime::component::{Component, Linker, ResourceTable};
+use wasmtime::{Config, Engine, Store};
+use wasmtime_wasi::{WasiCtx, WasiCtxBuilder, WasiCtxView, WasiView};
+
+struct HostState {
+    ctx: WasiCtx,
+    table: ResourceTable,
+}
+
+impl WasiView for HostState {
+    fn ctx(&mut self) -> WasiCtxView<'_> {
+        WasiCtxView {
+            ctx: &mut self.ctx,
+            table: &mut self.table,
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    let mut config = Config::new();
+    config.wasm_component_model(true);
+    let engine = Engine::new(&config)?;
+
+    let component = Component::from_file(&engine, "guest/target/wasm32-wasip1/release/guest.wasm")?;
+
+    let mut linker = Linker::<HostState>::new(&engine);
+    wasmtime_wasi::p2::add_to_linker_sync(&mut linker)?;
+
+    let state = HostState {
+        ctx: WasiCtxBuilder::new().inherit_stdio().build(),
+        table: ResourceTable::new(),
+    };
+    let mut store = Store::new(&engine, state);
+
+    let instance = linker.instantiate(&mut store, &component)?;
+
+    let func = instance.get_typed_func::<(), (String,)>(&mut store, "hello-world")?;
+    let (result,) = func.call(&mut store, ())?;
+
+    println!("{}", result);
+    Ok(())
 }
